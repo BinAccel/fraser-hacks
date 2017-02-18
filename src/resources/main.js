211c8aaeb29d906f0d1b78ccc6d73656c5750c38
jlsajfj <jlsajfj@hotmail.com>
2017-02-18 12:37:22 -0400

    Update main.js

--- src/resources/main.js
+++ src/resources/main.js
@@ -1,1 +1,16 @@
+function func()
+{
+  var city = ucwords(document.getElementById('location').value,true);
+  $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address="+city+"&key=AIzaSyBKZNpUw0X6MhSkHqrbWQSLi2OGpmR5-ms").then(function(data){
+    console.log(data);
+  }
+}
 
+//http://stackoverflow.com/questions/2017456/with-jquery-how-do-i-capitalize-the-first-letter-of-a-text-field-while-the-user
+function ucwords(str,force){
+  str=force ? str.toLowerCase() : str;  
+  return str.replace(/(\b)([a-zA-Z])/g,
+           function(firstLetter){
+              return   firstLetter.toUpperCase();
+           });
+}
