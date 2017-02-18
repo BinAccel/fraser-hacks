b43c142845fbd3209bf79ca1815cfd0e76d58124
Kirito <rogerjfu@hotmail.com>
2017-02-18 11:47:01 -0500

    Allow for 1 header
    @jlsajfj get `load_header.js` to work

---
+++ src/login.php
@@ -0,0 +1,12 @@
+<!Doctype HTML>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <script src="resoucres/load_header.js"></script>
+        <header class = "header"></header>
+        <!--Header goes here-->
+        
+    </body>
+</html>
